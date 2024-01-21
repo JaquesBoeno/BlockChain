a3f4e2602badd48edd0d936630a4dd0b7500af1b
JaquesBoeno <jaquesboeno@proton.me>
2024-01-21 11:58:12 -0300

    add calculateHash function

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"strings"
+)
+
 type Block struct {
 	Index     int
 	Timestamp string
@@ -9,6 +16,21 @@ type Block struct {
 }
 
 var BlockChain []Block
+
+func calculateHash(block Block) string {
+	record := strings.Builder{}
+	record.WriteString(fmt.Sprint(block.Index))
+	record.WriteString(block.Timestamp)
+	record.WriteString(fmt.Sprint(block.BPM))
+	record.WriteString(block.PrevHah)
+
+	h := sha256.New()
+	h.Write([]byte(record.String()))
+
+	hashed := h.Sum(nil)
+
+	return hex.EncodeToString(hashed)
+}
 
 func main() {
 
